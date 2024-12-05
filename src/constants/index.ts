9aa903864473da1f5ef4dc7854cc2bf5a281b902
Vladislav <vladislav.malchevskiy.work@gmail.com>
2024-12-05 18:13:57 +0300

    chore: create file for constants

---
+++ src/constants/index.ts
@@ -0,0 +1,14 @@
+const BREAKPOINTS = {
+  sm: "576px",
+  md: "768px",
+  lg: "992px",
+  xl: "1200px",
+};
+
+const LINK_PATH = {
+  HOME: "/",
+  TASKS: "/tasks",
+  CONTACTS: "/contacts",
+};
+
+export { BREAKPOINTS, LINK_PATH };
